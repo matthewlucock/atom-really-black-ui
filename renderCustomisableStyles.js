39720814348145f4cc0211df98340e4da8d621bf
matthewlucock <matthewlucock@icloud.com>
2018-07-02 11:52:15 +1000

    Refactor script for rendering customisable styles

---
+++ renderCustomisableStyles.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const path = require('path')
+
+const fse = require('fs-extra')
+const less = require('less')
+
+const DIRECTORY = 'styles/customisable'
+
+const FILES = [
+  'general',
+  'editor',
+  'image-background'
+]
+
+const DESTINATION_PATH = path.join(DIRECTORY, 'compiled.css')
+
+const getLessFilePath = fileName => path.join(DIRECTORY, `${fileName}.less`)
+
+const lessToRender = FILES
+  .map(fileName => fse.readFileSync(getLessFilePath(fileName), 'utf8'))
+  .join('')
+  .replace(/@([^\s;)]+)/g, '"$1"')
+
+;(async () => {
+  const {css} = await less.render(lessToRender, {compress: true})
+  await fse.writeFile(DESTINATION_PATH, css)
+})()
